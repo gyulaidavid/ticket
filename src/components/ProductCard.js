a9ed76f5a40ec655430875aeee13726b3175fb34
gyulaidavid <gyulaidavid@gmail.com>
2022-12-06 14:52:45 +0100

    swiperrel

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,12 +2,12 @@ import { Card, Button, Form, Row, Col } from 'react-bootstrap';
 import { CartContext } from '../CartContext';
 import { useContext } from 'react';
 //after install you need these elements for Swiper
-// import { Navigation, EffectFade } from "swiper";
-// import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, EffectFade } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/effect-fade"
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade"
 //after install you need these elements for Swiper END
 
 function ProductCard({product}) {
@@ -19,7 +19,7 @@ function ProductCard({product}) {
 
     return (
        
-        {/* <Swiper
+        <Swiper
         modules={[Navigation, EffectFade]}
         navigation
         effect
@@ -27,35 +27,35 @@ function ProductCard({product}) {
         slidesPerView={1}
         loop
         className
-        > */
-     /* {product.map((prod, i) => (
-         <SwiperSlide> */}
+        > 
+     {product.map((prod, i) => (
+         <SwiperSlide> *
         <Card>
             <Card.Body>
-                <Card.Title>{product.name}</Card.Title>
-                <Card.Text>{product.description}</Card.Text>
-                <Card.Text>${product.price}</Card.Text>
+                <Card.Title>{prod.name}</Card.Title>
+                <Card.Text>{prod.description}</Card.Text>
+                <Card.Text>${prod.price}</Card.Text>
                 { productQuantity > 0 ?
                     <>
                         <Form as={Row}>
                             <Form.Label column="true" sm="6">In Cart: {productQuantity}</Form.Label>
                             <Col sm="6">
-                                <Button sm="6" onClick={() => cart.addOneToCart(product.id)} className='mx-2'>+</Button>
-                                <Button sm="6" onClick={() => cart.removeOneFromCart(product.id)} className='mx-2'>-</Button>
+                                <Button sm="6" onClick={() => cart.addOneToCart(prod.id)} className='mx-2'>+</Button>
+                                <Button sm="6" onClick={() => cart.removeOneFromCart(prod.id)} className='mx-2'>-</Button>
                             </Col>
                         </Form>
-                        <Button variant="danger" onClick={() => cart.deleteFromCart(product.id)}>Remove</Button>
+                        <Button variant="danger" onClick={() => cart.deleteFromCart(prod.id)}>Remove</Button>
                     </>
                     :
                     
-                    <Button variant="primary" onClick={() => cart.addOneToCart(product.id)}> Add to Cart </Button>
+                    <Button variant="primary" onClick={() => cart.addOneToCart(prod.id)}> Add to Cart </Button>
                 }
                 
             </Card.Body>
         </Card>
-        {/* </SwiperSlide>
+        </SwiperSlide>
         ))}
-        </Swiper> */}
+        </Swiper>
        
     )
 }
